bc90a16d19342add01ea0a87a9ba59c51b999c42
Fabio Ventura <foravatenubi@gmail.com>
2024-03-24 20:12:39 +0100

    Add Account model

---
+++ database/migrations/2024_03_24_190845_create_accounts_table.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable(false);
+            $table->string('name')->nullable(false);
+            $table->enum('status', ['closed', 'open', 'highlight'])->nullable(false);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
