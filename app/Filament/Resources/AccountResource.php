94cd7e7ea40454aeaf57cc64a57eb0e3c66f1554
Fabio Ventura <foravatenubi@gmail.com>
2024-03-24 21:13:00 +0100

    Add Account resource

---
+++ app/Filament/Resources/AccountResource.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AccountResource\Pages;
+use App\Models\Account;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class AccountResource extends Resource
+{
+    protected static ?string $model = Account::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form->schema([
+            Forms\Components\TextInput::make('user_id')
+                ->required(),
+            Forms\Components\TextInput::make('name')
+                ->maxLength(255)
+                ->required(),
+            Forms\Components\Select::make('status')
+                ->options(['closes' => 'Closed', 'open' => 'Open', 'highlight' => 'Highlight'])
+                ->required(),
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('status'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAccounts::route('/'),
+            'create' => Pages\CreateAccount::route('/create'),
+            'edit' => Pages\EditAccount::route('/{record}/edit'),
+        ];
+    }
+}
