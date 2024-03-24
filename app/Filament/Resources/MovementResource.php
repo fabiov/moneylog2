b1f3e34f50c3c34bc4ee059ec9d171a3b8d1e2a2
Fabio Ventura <foravatenubi@gmail.com>
2024-03-24 21:56:06 +0100

    Add Movement resource

---
+++ app/Filament/Resources/MovementResource.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MovementResource\Pages;
+use App\Filament\Resources\MovementResource\RelationManagers;
+use App\Models\Movement;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class MovementResource extends Resource
+{
+    protected static ?string $model = Movement::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('amount')
+                    ->required()
+                    ->type('number')
+                    ->step(0.01),
+                Forms\Components\TextInput::make('description')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\DatePicker::make('date')
+                    ->required()
+                    ->maxDate(now()),
+                Forms\Components\Select::make('account_id')
+                    ->relationship('account', 'name')
+                    ->required(),
+                Forms\Components\Select::make('category_id')
+                    ->relationship('category', 'name')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('amount'),
+                Tables\Columns\TextColumn::make('date'),
+                Tables\Columns\TextColumn::make('description'),
+                Tables\Columns\TextColumn::make('account.name'),
+                Tables\Columns\TextColumn::make('category.name'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMovements::route('/'),
+            'create' => Pages\CreateMovement::route('/create'),
+            'edit' => Pages\EditMovement::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,6 +7,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Carbon;
 
 /**
@@ -23,5 +24,10 @@ class Category extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function movements(): HasMany
+    {
+        return $this->hasMany(Movement::class);
     }
 }
