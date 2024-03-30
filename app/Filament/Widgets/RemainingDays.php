2c129e13509d0b3d6b0301a17c032d58d82c5066
Fabio Ventura <foravatenubi@gmail.com>
2024-03-30 23:52:17 +0100

    Add remain budget

--- app/Filament/Widgets/RemainingDays.php
+++ app/Filament/Widgets/RemainingDays.php
@@ -6,21 +6,26 @@ namespace App\Filament\Widgets;
 
 use App\Models\Setting;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Illuminate\Support\Facades\DB;
 
 class RemainingDays extends BaseWidget
 {
     protected function getStats(): array
     {
+        $remainingBudget = $this->remainingBudget();
+
         return [
-            BaseWidget\Stat::make('Remaining to spend', '565,80 €')
+            BaseWidget\Stat::make('Remaining to spend',number_format($remainingBudget, 2, ',', '.') . ' €')
                 ->chart([1, 32, 54, 46])
                 ->chartColor('success')
-                ->color('danger')
-                ->description('descrizione')
-                ->descriptionIcon('heroicon-o-document-text'),
+                ->color($remainingBudget ? 'success' : 'danger')
+                ->description('Money left to spend in the month')
+                ->descriptionIcon('heroicon-o-currency-euro'),
+
             BaseWidget\Stat::make('Remaining to days', $this->remainingDays())
-                ->description('days remaining before the next paycheck')
+                ->description('Days remaining before the next paycheck')
                 ->descriptionIcon('heroicon-o-calendar'),
+
             BaseWidget\Stat::make('Daily budget', '47,15 €'),
         ];
     }
@@ -31,5 +36,16 @@ class RemainingDays extends BaseWidget
         $today = (int) date('j');
 
         return $today < $setting->payday ? $setting->payday - $today : intval(date('t')) - $today + $setting->payday;
+    }
+
+    private function remainingBudget(): float
+    {
+        $accountBalance = (float)DB::table('movements')
+            ->join('accounts', 'movements.account_id', '=', 'accounts.id')
+            ->where('accounts.user_id', '=', auth()->id())
+            ->where('accounts.status', '<>', 'closed')
+            ->sum('movements.amount');
+
+        return $accountBalance;
     }
 }
