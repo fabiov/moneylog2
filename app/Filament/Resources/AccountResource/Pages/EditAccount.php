168c1642dfd35276814dc41a5af78978bee99cd5
Fabio Ventura <foravatenubi@gmail.com>
2024-09-20 21:52:41 +0200

    Defined redirect url for edit pages

--- app/Filament/Resources/AccountResource/Pages/EditAccount.php
+++ app/Filament/Resources/AccountResource/Pages/EditAccount.php
@@ -16,4 +16,9 @@ class EditAccount extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
