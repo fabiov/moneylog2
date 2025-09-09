016f35f0558aefee31f6cc770ca2a06b36ae07ac
Fabio Ventura <foravatenubi@gmail.com>
2025-09-09 23:07:15 +0200

    Add schedule command

--- routes/console.php
+++ routes/console.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 use Illuminate\Foundation\Console\ClosureCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 /** @var ClosureCommand $this */
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Artisan::command('inspire', fn () => $this->comment(Inspiring::quote()))
+    ->purpose('Display an inspiring quote')
+    ->hourly();
+
+Schedule::command('emails:send Taylor --force')->daily();
