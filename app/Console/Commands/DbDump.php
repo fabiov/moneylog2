0698193167ebd1a1e07a979fe69766dc38631a0e
Fabio Ventura <foravatenubi@gmail.com>
2025-09-09 22:44:30 +0200

    Add dump upload on dropbox

--- app/Console/Commands/DbDump.php
+++ app/Console/Commands/DbDump.php
@@ -9,6 +9,7 @@ use Illuminate\Console\Command;
 use Spatie\DbDumper\Compressors\GzipCompressor;
 use Spatie\DbDumper\Databases\MySql;
 use Spatie\DbDumper\Exceptions\CannotSetParameter;
+use Spatie\Dropbox\Client;
 
 class DbDump extends Command
 {
@@ -16,20 +17,40 @@ class DbDump extends Command
     protected $signature = 'app:db-dump';
 
     /** @var string */
-    protected $description = 'DB dump';
+    protected $description = 'Make a DB dump adn upload it on dropbox';
 
     /**
      * @throws CannotSetParameter
      */
     public function handle(): int
     {
+        $filePath = tempnam(sys_get_temp_dir(), 'laravel_dump_');
+        if ($this->verbosity) {
+            $this->info("Creating temporary file $filePath");
+        }
+
         MySql::create()
             ->setDbName(Type::string(config('database.connections.mysql.database')))
             ->setUserName(Type::string(config('database.connections.mysql.username')))
             ->setPassword(Type::string(config('database.connections.mysql.password')))
             ->setHost(Type::string(config('database.connections.mysql.host')))
             ->useCompressor(new GzipCompressor)
-            ->dumpToFile(storage_path(sprintf('app/dump/moneylog2_%s.sql.gz', date('Y-m-d_H'))));
+            ->dumpToFile($filePath);
+
+        $resource = fopen($filePath, 'r');
+        if (! $resource) {
+            $this->error('Unable to create dump file.');
+
+            return self::FAILURE;
+        }
+
+        $client = new Client(Type::string(config('app.dropbox_token')));
+        try {
+            $client->upload(sprintf('moneylog2_%s.sql.gz', date('Y-m-d_H')), $resource);
+        } catch (\Exception $e) {
+            $this->error($e->getMessage());
+        }
+        unlink($filePath);
 
         return self::SUCCESS;
     }
